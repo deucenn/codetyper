bc8a50a8836ef54d1eff543569b8f364d316cc67
deucenn <panix.black@web.de>
2024-03-07 11:25:15 +0100

    qol changes

--- src/components/TypeBox.js
+++ src/components/TypeBox.js
@@ -2,33 +2,35 @@ import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
 import UserTypings from "./UserTypings";
 import codeSnippets from "./codeSnippets";
 
-const codeInput = {
-  name: "defer.js",
-  code: `
-    function defer(func, ...args) {
-      if (typeof func !== 'function') {
-        throw new TypeError('Expected a function')
-      }
-      return setTimeout(func, 1, ...args)
-    }`,
-  language: "javascript",
-};
+// const codeInput = {
+//   name: "defer.js",
+//   code: `
+//     function defer(func, ...args) {
+//       if (typeof func !== 'function') {
+//         throw new TypeError('Expected a function')
+//       }
+//       return setTimeout(func, 1, ...args)
+//     }`,
+//   language: "javascript",
+// };
 
 const Code = () => {
   return (
     <div className="relative p-4 rounded-2xl w-2/3 h-96">
-      <h2 className="text-xl italic text-slate-300 mb-5">{codeInput.name}</h2>
+      <h2 className="text-xl italic text-slate-300 mb-5">
+        {codeSnippets[0].name}
+      </h2>
       <div className="text-2xl text-slate-100 relative">
         <div className="absolute top-0 left-0 w-full h-full z-10 bg-white opacity-50">
           <UserTypings />
         </div>
         <SyntaxHighlighter
           useInlineStyles={false}
-          language={codeInput.language}
+          language={codeSnippets[0].language}
           lineProps={true}
           customStyle={{ textAlign: "left", zIndex: 0 }}
         >
-          {codeInput.code.trim()}
+          {codeSnippets[1].code.trim()}
         </SyntaxHighlighter>
       </div>
     </div>
--- src/components/UserTypings.js
+++ src/components/UserTypings.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import codeSnippets from "./codeSnippets";
 
 const UserTypings = () => {
   const [userInput, setUserInput] = useState("");
